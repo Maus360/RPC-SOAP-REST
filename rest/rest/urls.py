fdc0f570efb61710a0335deb35734661cae3ccc1
Maks <maksimochka200@gmail.com>
2019-04-07 20:40:31 +0300

    rewrite urls for project
    add admin page and redirect all urls for pascal app

--- rest/rest/urls.py
+++ rest/rest/urls.py
@@ -13,18 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rest.pascal import views
 
-router = routers.DefaultRouter()
-router.register(r"class", views.ClassViewSet)
-router.register(r"type", views.TypeViewSet)
-router.register(r"mathoperation", views.MathOperationViewSet)
-
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("rest.pascal.urls"))]
