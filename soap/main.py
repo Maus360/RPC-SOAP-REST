a74a342be49365f78ca53b6da7669244ca100c68
Maks <maksimochka200@gmail.com>
2019-04-11 23:40:33 +0300

    put wsgi server to main block

--- soap/main.py
+++ soap/main.py
@@ -294,11 +294,12 @@ handler = logging.FileHandler(
 )
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-from wsgiref.simple_server import make_server
-
-server = make_server(
-    server_config["soap"]["host"], server_config["soap"]["port"], wsgi_app
-)
 logger.info(f"Start SOAP server")
 print(f"Start SOAP server")
-server.serve_forever()
+if __name__ == "__main__":
+    from wsgiref.simple_server import make_server
+
+    server = make_server(
+        server_config["soap"]["host"], server_config["soap"]["port"], wsgi_app
+    )
+    server.serve_forever()
